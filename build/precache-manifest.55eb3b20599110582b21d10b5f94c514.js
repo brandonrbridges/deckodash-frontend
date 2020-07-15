8027e22fd9e3f15c42c1a54ce7b4434c860ed753
Brandon Bridges <brandon@visually.digital>
2020-07-15 21:12:31 +0100

    :+1: Complete Dev Build

---
+++ build/precache-manifest.55eb3b20599110582b21d10b5f94c514.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21abc08605f695febe575b55525cd63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47feca84921cd46cafc6",
+    "url": "/static/css/main.9c88f1dd.chunk.css"
+  },
+  {
+    "revision": "069b75aa805a0997f4bc",
+    "url": "/static/js/2.5f3cdb10.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.5f3cdb10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47feca84921cd46cafc6",
+    "url": "/static/js/main.468afebc.chunk.js"
+  },
+  {
+    "revision": "df575db3c957db3711f4",
+    "url": "/static/js/runtime-main.aff6920a.js"
+  },
+  {
+    "revision": "b4f9eb8ce027016ab9b9860817451d07",
+    "url": "/static/media/ProximaNovaExtraBold.b4f9eb8c.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/ProximaNovaRegular.410504d4.otf"
+  },
+  {
+    "revision": "8427e7b10fe9fc460b1717417bdc84ac",
+    "url": "/static/media/wood-grain.8427e7b1.svg"
+  }
+]);
